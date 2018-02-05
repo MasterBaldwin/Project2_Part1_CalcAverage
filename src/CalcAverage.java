5f7e548a9007e1260fc28acb2d92dad40e0b88a5
Mike Baldwin <Michael.Baldwin3@rockets.utoledo.edu>
2018-02-05 16:19:12 -0500

    Formatting changes.

--- src/CalcAverage.java
+++ src/CalcAverage.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
 
 public class CalcAverage {
-    // Written by: Mike Baldwin
-    // Computes the average of a set of values entered by the user, e.g. with
-    //     10.0 5.0 6.0 9.0 0.0
-    // The average is 7.5
+// Written by: Mike Baldwin
+// Computes the average of a set of values entered by the user, e.g. with
+//     10.0 5.0 6.0 9.0 0.0
+// The average is 7.5
 
-    public static void main(String[] args) {
-        Scanner stdIn = new Scanner(System.in);
-        int count;
-        double number, runningTotal;
+public static void main(String[] args) {
+    Scanner stdIn = new Scanner(System.in);
+    int count;
+    double number, runningTotal;
 
-        runningTotal = 0;
-        count = 0;
-        System.out.println("Type the numbers, the last being 0");
+    runningTotal = 0;
+    count = 0;
+    System.out.println("Type the numbers, the last being 0");
 
+    number = stdIn.nextDouble();
+    while (number != 0) {
+        runningTotal = runningTotal + number;
+        count = count + 1;
         number = stdIn.nextDouble();
-        while (number != 0) {
-            runningTotal = runningTotal + number;
-            count = count + 1;
-            number = stdIn.nextDouble();
-        }
-        System.out.print("The average of the ");
-        System.out.print(count);
-        System.out.print(" numbers is ");
-        System.out.println(runningTotal/count);
     }
+    System.out.print("The average of the ");
+    System.out.print(count);
+    System.out.print(" numbers is ");
+    System.out.println(runningTotal / count);
+}
 }
